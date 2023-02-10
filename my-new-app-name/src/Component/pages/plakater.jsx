08b18ac4ed6918322dc12eec927d57f7b08fb62d
Jacob <jacobsimoninielsen@gmail.com>
2023-02-10 11:12:15 +0100

    Update plakater.jsx

--- my-new-app-name/src/Component/pages/plakater.jsx
+++ my-new-app-name/src/Component/pages/plakater.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { PageHeader } from '../App/pageHeader/pageHeader'
 import { PlakaterStyled } from '../Style/plakater.style'
-import { useState } from 'react'
 import axios from 'axios'
 
 export const Plakater = () => {
